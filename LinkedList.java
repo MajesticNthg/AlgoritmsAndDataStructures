12406f937923a25e5b812db1ca4fccde112ad31f
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-08-25 00:56:41 +1000

    Update LinkedList.java
    UPD correct tail in 'insertAfter method', if 'nodeAfter == null'

--- LinkedList.java
+++ LinkedList.java
@@ -101,6 +101,7 @@ public class LinkedList {
         if (_nodeAfter == null) {
             _nodeToInsert.next = this.head;
             this.head = _nodeToInsert;
+            this.tail = _nodeToInsert;
             return;
         }
 
