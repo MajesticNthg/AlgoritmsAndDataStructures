a3244160e2b1c8c9f06aa520c10ec7b08cc6202b
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-05 00:00:22 +1000

    Create project DynamicMassive

---
+++ DynArray/DynArray.java
@@ -0,0 +1,82 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class DynArray<T> {
+    public T [] array;
+    public int count;
+    public int capacity;
+    Class clazz;
+
+    public DynArray (Class clz) {
+        clazz = clz;
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray (int new_capacity) {
+        if (new_capacity < 16) {
+            capacity = 16;
+            return;
+        } else {
+            capacity = new_capacity;
+        }
+
+        if (count == 0) {
+            array = (T[]) Array.newInstance(this.clazz, new_capacity);
+        } else {
+            array = Arrays.copyOf(array, new_capacity);
+        }
+    }
+
+    public T getItem (int index) throws Exception {
+        if (index > count || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Error, index is not correct");
+        }
+        return array[index];
+    }
+
+    public void append (T itm) {
+        capacitySize();
+
+        int lastElement = count + 1;
+        array[lastElement] = itm;
+    }
+
+    public void insert (T itm, int index) {
+        capacitySize();
+
+        if (index == count) {
+            append(itm);
+            return;
+        }
+
+        for (int i = count; i > index; i--) {
+            array[i] = array[i--];
+        }
+        array[index] = itm;
+    }
+
+    public void remove (int index) throws Exception {
+        if (index < 0 || index > count) {
+            throw new ArrayIndexOutOfBoundsException("Index is not correct");
+        }
+
+        for (int i = index; i < count; i++) {
+            array[i] = array[i++];
+        }
+        array[count] = null;
+
+        if (capacity > 16 && count < capacity / 2) {
+            makeArray((int)(capacity / 1.5));
+        }
+        if (capacity < 16) {
+            capacity = 16;
+        }
+    }
+
+    private void capacitySize() {
+        if (count >= capacity) {
+            makeArray(capacity * 2);
+        }
+    }
+}
