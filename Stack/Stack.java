0cd18022829c04e2cc3fd750b09e4267db5a2a01
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-17 17:37:41 +1000

    Update Stack.java
    Correct 'push method' and add two methods - 'getMin' and 'getAverage'

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,11 +1,16 @@
 import java.util.*;
+
 public class Stack<T> {
     private LinkedList<T> list;
+    private LinkedList<T> minStack;
     private int size;
+    private double sum;
 
     public Stack() {
         this.list = new LinkedList<>();
+        this.minStack = new LinkedList<>();
         this.size = 0;
+        this.sum = 0;
     }
 
     public int size() {
@@ -24,6 +29,11 @@ public class Stack<T> {
     public void push (T val) {
         this.list.addFirst(val);
         this.size++;
+
+        this.sum += (Integer)val;
+        if (minStack.isEmpty() || (Integer)val < (Integer)minStack.peek()) {
+            minStack.push(val);
+        }
     }
 
     public T peek () {
@@ -32,5 +42,12 @@ public class Stack<T> {
         }
 
         return this.list.peekFirst();
+    }
+    public T getMin () {
+        return minStack.peek();
+    }
+
+    public double getAverage () {
+        return sum / list.size();
     }
 }
