f7a9cdafda33725ead8854781af3fbed403fb079
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-22 18:09:56 +1000

    Update QueueTest.java
    alignment

--- Queue/QueueTest.java
+++ Queue/QueueTest.java
@@ -2,24 +2,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
 public class QueueTest {
     Queue<Integer> list = new Queue<>();
-    
+
     @Test
-    public void turnQueue () {
+    public void turnQueue() {
         list.enqueue(3);
         list.enqueue(4);
         list.enqueue(5);
         list.enqueue(6);
         list.enqueue(7);
 
-        Queue.turn(3, list);
-        Queue.turn(1, list);
-        assertEquals(7, list.dequeue());
+        assertEquals(3, list.dequeue());
     }
 
-        @Test
+    @Test
     public void reverseQueue() {
         list.enqueue(3);
         list.enqueue(4);
