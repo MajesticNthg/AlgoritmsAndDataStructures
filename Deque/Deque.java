55f0e0994f967d459a3b04fb4df8792a9b279451
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-22 23:14:46 +1000

    Create project Deque

---
+++ Deque/Deque.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Deque<T> {
+    private LinkedList<T> list;
+    private int size;
+
+    public Deque() {
+        this.list = new LinkedList<>();
+        this.size = 0;
+    }
+
+    public void addFront (T item) {
+        this.list.addFirst(item);
+        this.size++;
+    }
+
+    public void addTail (T item) {
+        this.list.addLast(item);
+        this.size++;
+    }
+
+    public T removeFront () {
+        if (this.list.isEmpty()) {
+            return null;
+        }
+
+        this.size--;
+
+        this.list.removeFirst();
+    }
+
+    public T removeTail () {
+        if (this.list.isEmpty()) {
+            return null;
+        }
+
+        this.size--;
+
+        return this.list.removeLast();
+    }
+
+    public int size () {
+        return this.list.size();
+    }
+
+}
