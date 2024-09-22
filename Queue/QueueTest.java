20b0aaa6308e65281f307be2209070614af88973
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-22 16:20:29 +1000

    Create test Queue

---
+++ Queue/QueueTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.*;
+public class QueueTest {
+    Queue<Integer> list = new Queue<>();
+    
+    @Test
+    public void turnQueue () {
+        list.enqueue(3);
+        list.enqueue(4);
+        list.enqueue(5);
+        list.enqueue(6);
+        list.enqueue(7);
+
+        Queue.turn(3, list);
+        Queue.turn(1, list);
+        assertEquals(7, list.dequeue());
+    }
+}
