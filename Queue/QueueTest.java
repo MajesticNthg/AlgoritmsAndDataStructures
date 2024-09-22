8fad810e9e395c8acce5d3090f6efcb0fe3c181b
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-09-22 18:08:50 +1000

    Update QueueTest.java
    Add test reverseFunction

--- Queue/QueueTest.java
+++ Queue/QueueTest.java
@@ -18,4 +18,17 @@ public class QueueTest {
         Queue.turn(1, list);
         assertEquals(7, list.dequeue());
     }
+
+        @Test
+    public void reverseQueue() {
+        list.enqueue(3);
+        list.enqueue(4);
+        list.enqueue(5);
+        list.enqueue(6);
+        list.enqueue(7);
+
+        list.reverseQueue(this.list);
+        assertEquals(7, list.dequeue());
+        assertEquals(6, list.dequeue());
+    }
 }
