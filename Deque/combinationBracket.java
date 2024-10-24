c9c064941dabee5096c699a834af008cdad43eb0
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-10-25 03:31:29 +1000

    Update combinationBracket.java
    Change body method. Use hashmap for keep bracket

--- Deque/combinationBracket.java
+++ Deque/combinationBracket.java
@@ -1,25 +1,41 @@
-public class combinationBracket {
-    public boolean combinationBracket (String s) {
+    public boolean combinationBracket(String s) {
         Deque<Character> bracket = new Deque<>();
+        Map<Integer, Character> myMap = new HashMap<>();
+        myMap.put(1, '(');
+        myMap.put(2, ')');
+        myMap.put(3, '[');
+        myMap.put(4, ']');
+        myMap.put(5, '{');
+        myMap.put(6, '}');
+
         for (int x = 0; x < s.length(); x++) {
-            if (s.charAt(x) == '(' || s.charAt(x) == '[' || s.charAt(x) == '{') {
-                bracket.addTail(s.charAt(x));
-                continue;
-            }
-            Character symbol = bracket.removeTail();
-            if (symbol == null) {
+            if (!searchBracket(s.charAt(x), myMap, bracket)) {
                 return false;
             }
-            if (s.charAt(x) == ')' && symbol != '(') {
+        }
+
+        if (bracket.size() != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean searchBracket(Character symbol, Map myMap, Deque<Character> bracket) {
+        for (int x = 1; x <= myMap.size(); x++) {
+            if (symbol == myMap.get(x) && x % 2 != 0) {
+                bracket.addTail(symbol);
+                return true;
+            }
+            if (symbol == myMap.get(x) && bracket.size() == 0) {
                 return false;
             }
-            if (s.charAt(x) == ']' && symbol != '[') {
-                return false;
-            }
-            if (s.charAt(x) == '}' && symbol != '{') {
-                return false;
+            if (symbol == myMap.get(x)) {
+                Character check = bracket.removeTail();
+                if (check != myMap.get(x - 1)) {
+                    return false;
+                }
             }
         }
         return true;
     }
-}
