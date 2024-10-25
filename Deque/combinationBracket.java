bed9da7d86cc7a41170f376a1cbe6e44b307c1bc
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-10-25 22:09:00 +1000

    Update combinationBracket.java
    Update combinationBracket (change keys (number on bracket), update loop (for each)).

--- Deque/combinationBracket.java
+++ Deque/combinationBracket.java
@@ -1,12 +1,9 @@
     public boolean combinationBracket(String s) {
         Deque<Character> bracket = new Deque<>();
         Map<Integer, Character> myMap = new HashMap<>();
-        myMap.put(1, '(');
-        myMap.put(2, ')');
-        myMap.put(3, '[');
-        myMap.put(4, ']');
-        myMap.put(5, '{');
-        myMap.put(6, '}');
+        myMap.put('(', ')');
+        myMap.put('[', ']');
+        myMap.put('{', '}');
 
         for (int x = 0; x < s.length(); x++) {
             if (!searchBracket(s.charAt(x), myMap, bracket)) {
@@ -21,18 +18,18 @@
         return true;
     }
 
-    private boolean searchBracket(Character symbol, Map myMap, Deque<Character> bracket) {
-        for (int x = 1; x <= myMap.size(); x++) {
-            if (symbol == myMap.get(x) && x % 2 != 0) {
+    private boolean searchBracket(Character symbol, Map<Character, Character> myMap, Deque<Character> bracket) {
+        for (Map.Entry<Character, Character> entry : myMap.entrySet()) {
+            if (symbol == entry.getKey()) {
                 bracket.addTail(symbol);
                 return true;
             }
-            if (symbol == myMap.get(x) && bracket.size() == 0) {
+            if (symbol == entry.getValue() && bracket.size() == 0) {
                 return false;
             }
-            if (symbol == myMap.get(x)) {
+            if (symbol == entry.getValue()) {
                 Character check = bracket.removeTail();
-                if (check != myMap.get(x - 1)) {
+                if (check != entry.getKey()) {
                     return false;
                 }
             }
