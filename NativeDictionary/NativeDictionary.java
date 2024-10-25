8e5b790e98d408294e7626e06064a89e797f6c82
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-10-26 02:47:48 +1000

    Update NativeDictionary.java
    Update without while

--- NativeDictionary/NativeDictionary.java
+++ NativeDictionary/NativeDictionary.java
@@ -4,15 +4,15 @@ import java.nio.charset.StandardCharsets;
 class NativeDictionary<T> {
     public int size;
     public String[] slots;
-    public T [] values;
+    public T[] values;
 
-    public NativeDictionary (int sz, Class clazz) {
-        size = sz;
-        slots = new String[size];
-        values = (T[]) Array.newInstance(clazz, this.size);
+    public NativeDictionary(int size, Class clazz) {
+        this.size = size;
+        this.slots = new String[size];
+        this.values = (T[]) Array.newInstance(clazz, this.size);
     }
 
-    public int hashFun (String key) {
+    private int hashFun(String key) {
         byte[] bytes = key.getBytes(StandardCharsets.UTF_8);
 
         int sum = 0;
@@ -23,35 +23,85 @@ class NativeDictionary<T> {
         return sum % this.size;
     }
 
-    public boolean isKey (String key) {
-        return (find(key) != -1);
-    }
-
-    public void put (String key, T value) {
-
-    }
-
-    public T get (String key) {
-        return null;
+    public boolean isKey(String key) {
+        return find(key) > -1;
     }
 
     private int find(String value) {
         int index = hashFun(value);
         int i = index;
 
-        do {
-            if (this.slots[i] == null) {
-                return -1;
-            }
-
-            if (this.slots[i].equals(value)) {
-                return i;
-            }
-
+        int z = check(i, value);
+        if (z == -1 || z == i) {
+            return z;
+        }
+        else {
             i = stepIndex(i);
-        } while (i != index);
+        }
+
+        for (int x = i; i != index; i = stepIndex(i)) {
+            if (check(i, value) == -1 || check(i, value) == i) {
+                return check(i, value);
+            }
+        }
 
         return -1;
+    }
+    private int check (int i, String value) {
+        if (this.slots[i] == null) {
+            return -1;
+        }
+        if (this.slots[i].equals(value)) {
+            return i;
+        }
+        return -2;
+    }
+
+    public void put(String key, T value) {
+        int index = seekSlot(key);
+
+        if (index > -1) {
+            this.slots[index] = key;
+            this.values[index] = value;
+        }
+    }
+
+    public T get(String key) {
+        int index = find(key);
+
+        if (index > -1) {
+            return values[index];
+        }
+
+        return null;
+    }
+
+    private int seekSlot(String value) {
+        int index = hashFun(value);
+        int i = index;
+
+        int z = secondCheck(i, value);
+        if (z == i) {
+            return i;
+        }
+        else {
+            i = stepIndex(i);
+        }
+
+
+        for (int x = i; i != index; i = stepIndex(i)) {
+            if (secondCheck(i, value) == i) {
+                return secondCheck(i, value);
+            }
+        }
+
+        return -1;
+    }
+    private int secondCheck (int i, String value) {
+        if (this.slots[i] == null) {
+            return i;
+        }
+        return -2;
     }
 
     private int stepIndex(int index) {
@@ -62,20 +112,5 @@ class NativeDictionary<T> {
         }
 
         return index;
-    }
-
-    public int seekSlot (String value) {
-        int index = hashFun(value);
-        int i = index;
-
-        do {
-            if (this.slots[i] == null) {
-                return i;
-            }
-
-            i = stepIndex(i);
-        } while (i != index);
-
-        return -1;
     }
 }
