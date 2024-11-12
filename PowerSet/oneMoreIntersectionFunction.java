992194f93f327a5d4045a5777b156b4341637fc8
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-11-12 21:00:49 +1000

    Update oneMoreIntersectionFunction.java
    Update for set class (not PowerSet) using method 'retainAll()'

--- PowerSet/oneMoreIntersectionFunction.java
+++ PowerSet/oneMoreIntersectionFunction.java
@@ -1,38 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class oneMoreIntersectionFunction {
-    public PowerSet moreIntersection(ArrayList<PowerSet> listPowerSet) {
-        PowerSet emptySet = new PowerSet();
-        PowerSet first = listPowerSet.getFirst();
-        PowerSet second = listPowerSet.get(1);
-        PowerSet main = oneMoreIntersection(first, second);
-
-        if (main.size() == 0) {
-            return emptySet;
-        }
+    public Set moreIntersection(ArrayList<Set> listPowerSet) {
+        Set emptySet = new HashSet<>();
+        Set main = new HashSet<>(listPowerSet.getFirst());
 
         for (int i = 1; i < listPowerSet.size(); i++) {
-            main = oneMoreIntersection(main, listPowerSet.get(i));
-            if (main.size() == 0) {
+            main.retainAll(listPowerSet.get(i));
+            if (main.isEmpty()) {
                 return emptySet;
             }
         }
         return main;
-    }
-
-    private PowerSet oneMoreIntersection(PowerSet set1, PowerSet set2) {
-        PowerSet intersectionSet = new PowerSet();
-
-        for (int i = 0; i < set1.size; i++) {
-            if (set1.slots[i] == null) {
-                continue;
-            }
-
-            if (set2.get(set1.slots[i])) {
-                intersectionSet.put(set1.slots[i]);
-            }
-        }
-
-        return intersectionSet;
     }
 }
